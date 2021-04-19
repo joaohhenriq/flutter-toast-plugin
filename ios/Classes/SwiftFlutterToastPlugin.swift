45513cdf13f8a2705cfb5a59f61e3a58df661d32
joaohhenriq <joao_camargo10@hotmail.com>
2021-04-19 20:17:20 -0300

    iOS implementation for Toast

--- ios/Classes/SwiftFlutterToastPlugin.swift
+++ ios/Classes/SwiftFlutterToastPlugin.swift
@@ -9,6 +9,14 @@ public class SwiftFlutterToastPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    if(call.method.elementsEqual("showToast")) {
+        let arguments = call.arguments as? NSDictionary
+        let msg = arguments!["msg"] as? String
+
+        UIAlertView(title: "", message: msg, delegate: nil, cancelButtonTitle: "OK").show()
+    }
+
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
